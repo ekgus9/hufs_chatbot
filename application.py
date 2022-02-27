18b5c9b64699f2c2c48de5572a9c32cca2c32aee
Dahyun Jung <dhaabb55@naver.com>
2022-02-27 11:09:54 +0900

    Add files via upload

---
+++ application.py
@@ -0,0 +1,225 @@
+from flask import Flask, jsonify
+import sys
+import requests
+import re
+from bs4 import BeautifulSoup
+from menu import menu_
+from notice import notice_5
+from weather import todayWeather, nextWeather
+application = Flask(__name__)
+
+@application.route("/schedule",methods=['POST'])
+def schedule():
+    
+    url = 'https://builder.hufs.ac.kr/user/indexSub.action?codyMenuSeq=37069&siteId=hufs&menuType=T&uId=4&sortChar=AAA&menuFrame=&linkUrl=04_0101.html&mainFrame=right'
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        title = soup.select_one('#hu_cont_mid > div > div > div:nth-child(7) > div.sch02_box.mb10 > table')
+        latter = title.get_text()
+        text = re.compile('\w.+\w')
+        latter = text.findall(latter)
+        
+        output = '이번 달 학사일정입니다!\n\n'
+        
+        for l in range(len(latter)):
+            if l % 2 == 0: output = output + latter[l]
+            else: output = output + ' ' + latter[l] + '\n'
+                
+        res = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "basicCard": {
+                            "title": "",
+                            "description": output,
+                            "thumbnail": {
+                                "imageUrl": ""
+                            },
+                            "buttons": [
+                                {
+                                    "action": "webLink",
+                                    "label": "전체 일정보기",
+                                    "webLinkUrl": url
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+        
+        return jsonify(res)
+    
+@application.route("/inmunmenu",methods=['POST'])
+def inmunmenu():
+    res = {
+    "version": "2.0",
+    "template": {
+        "outputs": [
+            {
+                "simpleText": {
+                    "text": menu_('inmun',5)
+                }
+            }
+        ]
+    }
+}
+    return jsonify(res)
+
+@application.route("/skymenu",methods=['POST'])
+def skymenu():
+    res = {
+    "version": "2.0",
+    "template": {
+        "outputs": [
+            {
+                "simpleText": {
+                    "text": menu_('sky',1)
+                }
+            }
+        ]
+    }
+}
+    return jsonify(res)
+
+@application.route("/promenu",methods=['POST'])
+def promenu():
+    res = {
+    "version": "2.0",
+    "template": {
+        "outputs": [
+            {
+                "simpleText": {
+                    "text": menu_('pro',2)
+                }
+            }
+        ]
+    }
+}
+    return jsonify(res)
+
+@application.route("/hugomenu",methods=['POST'])
+def hugomenu():
+    res = {
+    "version": "2.0",
+    "template": {
+        "outputs": [
+            {
+                "simpleText": {
+                    "text": menu_('hugo',1)
+                }
+            }
+        ]
+    }
+}
+    return jsonify(res)
+
+@application.route("/domenu",methods=['POST'])
+def domenu():
+    res = {
+    "version": "2.0",
+    "template": {
+        "outputs": [
+            {
+                "simpleText": {
+                    "text": menu_('do',4)
+                }
+            }
+        ]
+    }
+}
+    return jsonify(res)
+
+@application.route("/umenu",methods=['POST'])
+def umenu():
+    res = {
+    "version": "2.0",
+    "template": {
+        "outputs": [
+            {
+                "simpleText": {
+                    "text": menu_('u',1)
+                }
+            }
+        ]
+    }
+}
+    return jsonify(res)
+
+@application.route("/humenu",methods=['POST'])
+def humenu():
+    res = {
+    "version": "2.0",
+    "template": {
+        "outputs": [
+            {
+                "simpleText": {
+                    "text": menu_('hu',1)
+                }
+            }
+        ]
+    }
+}
+    return jsonify(res)
+
+@application.route("/unimenu",methods=['POST'])
+def unimenu():
+    res = {
+    "version": "2.0",
+    "template": {
+        "outputs": [
+            {
+                "simpleText": {
+                    "text": menu_('uni',1)
+                }
+            }
+        ]
+    }
+}
+    return jsonify(res)
+
+@application.route("/notice",methods=['POST'])
+def notice():
+    return notice_5()
+
+@application.route("/todayweather",methods=['POST'])
+def todayweather():
+    
+    res = {
+    "version": "2.0",
+    "template": {
+        "outputs": [
+            {
+                "simpleText": {
+                    "text": todayWeather()
+                }
+            }
+        ]
+    }
+}
+    return jsonify(res)
+
+@application.route("/nextweather",methods=['POST'])
+def nextweather():
+    
+    res = {
+    "version": "2.0",
+    "template": {
+        "outputs": [
+            {
+                "simpleText": {
+                    "text": nextWeather()
+                }
+            }
+        ]
+    }
+}
+    return jsonify(res)
+    
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0', port=5000, threaded=True)
---
+++ weather.py
@@ -0,0 +1,71 @@
+from flask import Flask, jsonify
+from bs4 import BeautifulSoup
+import requests
+
+#@app.route("/weather")
+def todayWeather(where):
+    
+    seoul_url = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%EC%9D%B4%EB%AC%B8%EB%8F%99+%EB%82%A0%EC%94%A8'
+    mohyeon_url = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EB%AA%A8%ED%98%84+%EB%82%A0%EC%94%A8'
+    
+    if where == 's': url = seoul_url
+    elif where == 'm' : url = mohyeon_url
+    
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        
+        title = soup.select_one('#main_pack > section.sc_new.cs_weather_new._cs_weather > div._tab_flicking > div.content_wrap > div.open > div:nth-child(1) > div > div.weather_info > div')
+        weather_text = title.text.split()
+        
+        if '구름많음' == weather_text[2] or '흐림' == weather_text[2]: weather_text.append('☁️')
+        elif '맑음' == weather_text[2]: weather_text.append('🌞')
+        
+        res = '''이문동 오늘 날씨{}\n
+현재 온도 {}
+{}\n
+강수확률 {}
+습도 {}
+바람 {}
+미세먼지 {}
+초미세먼지 {}'''.format(weather_text[-1],weather_text[4][2:],weather_text[2],\
+            weather_text[10],weather_text[12],weather_text[14],weather_text[16],weather_text[18])
+        
+    return res
+
+#@app.route("/nextweather")
+def nextWeather(where):
+    
+    seoul_url = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%EB%82%B4%EC%9D%BC+%EC%9D%B4%EB%AC%B8%EB%8F%99+%EB%82%A0%EC%94%A8'
+    mohyeon_url = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%EB%82%B4%EC%9D%BC+%EB%AA%A8%ED%98%84+%EB%82%A0%EC%94%A8'
+    
+    if where == 's': url = seoul_url
+    elif where == 'm' : url = mohyeon_url
+    
+    response = requests.get(seoul_url)
+    
+    if response.status_code == 200:
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        
+        title = soup.select_one('#main_pack > section.sc_new.cs_weather_new._cs_weather > div._tab_flicking > div.content_wrap > div.open > div:nth-child(1) > div > div.weather_info.type_tomorrow > div > ul')
+        weather_text = title.text.split()
+        
+        res = '''이문동 내일 날씨\n
+-오전-
+온도 {}
+{}
+강수확률 {}
+미세먼지 {}
+초미세먼지 {}
+\n-오후-
+온도 {}
+{}
+강수확률 {}
+미세먼지 {}
+초미세먼지 {}\n'''.format(weather_text[3][2:],weather_text[4],weather_text[6],weather_text[8],weather_text[10],\
+    weather_text[14][2:],weather_text[15],weather_text[17],weather_text[19],weather_text[21])
+        
+    return res
