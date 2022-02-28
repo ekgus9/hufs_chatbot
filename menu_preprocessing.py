4d1a2d3c5663b643e677d72b9b15c10536845d4e
Dahyun Jung <dhaabb55@naver.com>
2022-02-28 22:01:08 +0900

    Update menu_preprocessing.py

--- menu_preprocessing.py
+++ menu_preprocessing.py
@@ -1,9 +1,5 @@
 import re
 def preprocessing(t):
-    t_re = re.compile('[0-9],[0-9]+원')
-    t = t_re.sub('',t)
-    t_re = re.compile('[0-9]+ Kcal')
-    t = t_re.sub('',t)
-    t = t.split('\n')
-    t = [i for i in t if (':' not in i) and ('~' not in i) and ('' != i)]
-    return '\n'.join(t)
+    t_re = re.compile('\n+')
+    t = t_re.sub('\n',t)
+    return t
